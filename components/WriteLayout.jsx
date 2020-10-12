9483d1f5ae239ddbb9fd9ea29a8ab13a7c71c98d
unknown <dudtjr913@naver.com>
2020-10-12 16:43:33 +0900

    pra

--- components/WriteLayout.jsx
+++ components/WriteLayout.jsx
@@ -4,6 +4,14 @@ import {
   FileImageOutlined,
   VideoCameraOutlined,
   LinkOutlined,
+  DownOutlined,
+  BoldOutlined,
+  UnderlineOutlined,
+  ItalicOutlined,
+  FontColorsOutlined,
+  BgColorsOutlined,
+  AlignLeftOutlined,
+  LineHeightOutlined,
 } from '@ant-design/icons';
 import styled from 'styled-components';
 
@@ -19,31 +27,33 @@ const ButtonWrapper = styled.button`
   }
 `;
 
+const UlWrapper = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 5px 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+`;
+
 const AppLayout = ({ children }) => {
   return (
     <>
-      <header style={{ margin: '5px 15px', minHeight: '4vh' }}>
+      <header style={{ padding: '5px 0px', minHeight: '4vh' }}>
         <div
           style={{
             borderBottom: '1px solid lightgray',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
+            padding: '0px 15px',
             paddingBottom: '5px',
           }}
         >
           <Button>홈</Button>
           <Button style={{ marginRight: '10px' }}>저장</Button>
         </div>
-        <ul
-          style={{
-            listStyle: 'none',
-            padding: '0',
-            marginTop: '10px',
-            display: 'flex',
-            borderBottom: '1px solid lightgray',
-          }}
-        >
+        <UlWrapper>
           <li>
             <ButtonWrapper>
               <FileImageOutlined style={{ fontSize: '25px' }} />
@@ -62,7 +72,117 @@ const AppLayout = ({ children }) => {
               <div>링크</div>
             </ButtonWrapper>
           </li>
-        </ul>
+        </UlWrapper>
+        <UlWrapper>
+          <li>
+            <ButtonWrapper>
+              <span>본문</span>
+              <DownOutlined
+                style={{
+                  fontSize: '10px',
+                  marginLeft: '10px',
+                  color: 'lightgray',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <span>나눔고딕</span>
+              <DownOutlined
+                style={{
+                  fontSize: '10px',
+                  marginLeft: '10px',
+                  color: 'lightgray',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <span>15</span>
+              <DownOutlined
+                style={{
+                  fontSize: '10px',
+                  marginLeft: '10px',
+                  color: 'lightgray',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <BoldOutlined
+                style={{
+                  borderLeft: '1px solid lightgray',
+                  fontSize: '15px',
+                  paddingLeft: '15px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <ItalicOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '5px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <UnderlineOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '5px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <FontColorsOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '5px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <BgColorsOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '5px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <AlignLeftOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '10px',
+                  borderLeft: '1px solid lightgray',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+          <li>
+            <ButtonWrapper>
+              <LineHeightOutlined
+                style={{
+                  fontSize: '15px',
+                  paddingLeft: '5px',
+                }}
+              />
+            </ButtonWrapper>
+          </li>
+        </UlWrapper>
       </header>
       <main style={{ margin: '0 auto' }}>
         <Row>

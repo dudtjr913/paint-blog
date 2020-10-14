6be8cd6749af0cf4e27c827d7d3b534f948b2dda
unknown <dudtjr913@naver.com>
2020-10-14 17:11:52 +0900

    pra

--- components/ProfileForm.jsx
+++ components/ProfileForm.jsx
@@ -10,45 +10,47 @@ const ProfileForm = () => {
       style={{
         minHeight: 'calc(100vh - 48.44px)',
         textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
     >
-      <Row>
-        <Col xs={24} md={12}>
-          <Card title="프로필" style={{ width: '45vw', height: '250px' }}>
-            <Card.Meta
-              avatar={<Avatar icon={<UserOutlined />} size={60} />}
-              title={
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'flex-start',
-                  }}
-                >
-                  <div>게시글 : {contents.length}</div>
-                  <div>닉네임 : {<span>히밤</span>}</div>
-                </div>
-              }
-            />
-            <Button
-              style={{ position: 'absolute', left: '24px', marginTop: '60px' }}
-            >
-              수정
-            </Button>
-          </Card>
-        </Col>
-        <Col xs={24} md={12}>
-          <Card title="내정보" style={{ width: '45vw', height: '250px' }}>
-            <div>이메일</div>
-            <div>전화번호</div>
-            <Button
-              style={{ position: 'absolute', left: '24px', marginTop: '60px' }}
-            >
-              수정
-            </Button>
-          </Card>
-        </Col>
-      </Row>
+      <Col xs={24} md={12}>
+        <Card
+          title="프로필"
+          style={{
+            width: '20vw',
+            height: '250px',
+            float: 'left',
+          }}
+        >
+          <Card.Meta
+            avatar={<Avatar icon={<UserOutlined />} size={60} />}
+            title={
+              <div>
+                <div>게시글 : {contents.length}</div>
+                <div>닉네임 : {<span>히밤</span>}</div>
+              </div>
+            }
+          />
+          <Button
+            style={{ position: 'absolute', left: '24px', marginTop: '60px' }}
+          >
+            수정
+          </Button>
+        </Card>
+      </Col>
+      <Col xs={24} md={12}>
+        <Card title="내정보" style={{ width: '20vw', height: '250px' }}>
+          <div>이메일</div>
+          <div>전화번호</div>
+          <Button
+            style={{ position: 'absolute', left: '24px', marginTop: '60px' }}
+          >
+            수정
+          </Button>
+        </Card>
+      </Col>
     </section>
   );
 };
